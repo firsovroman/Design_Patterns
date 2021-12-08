162e9bec24c354e9f187293cbf6345e5dd90e283
firsovroman <romanfirs@list.ru>
2021-12-08 22:10:05 +0800

    .

---
+++ src/main/java/com/firsov/design_patterns/H_facade/Job.java
@@ -0,0 +1,7 @@
+package com.firsov.design_patterns.H_facade;
+
+public class Job {
+    public void doJob() {
+        System.out.println("Job in progress...");
+    }
+}
---
+++ src/main/java/com/firsov/design_patterns/H_facade/WorkflowFacade.java
@@ -0,0 +1,15 @@
+package com.firsov.design_patterns.H_facade;
+
+public class WorkflowFacade {
+
+    Developer developer = new Developer();
+    Job job = new Job();
+    BugTracker bugTracker = new BugTracker();
+
+    public void solveProblems() {
+        job.doJob();
+        bugTracker.startSprint();
+        developer.doJobBeforeDeadline(bugTracker);
+    }
+
+}
